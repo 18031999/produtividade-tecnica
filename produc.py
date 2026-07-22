0724ec77b6bd046427412c5ca51cbfbafbaf2721
18031999 <58603394+18031999@users.noreply.github.com>
2026-07-22 09:20:16 -0300

    Update produc.py

--- produc.py
+++ produc.py
@@ -11,7 +11,7 @@ st.title("📋Produtividade HHP")
 SENHA_SUPERVISOR = "admin123"
 
 # --- LISTAS OFICIAIS ---
-TECNICOS = ["Ludian", "João da Hora", "Erison", "Leonardo", "Tomé","Gabrielli", "Gabriel", "Vagner"]
+TECNICOS = ["Ludian", "João da Hora", "Erison", "Leonardo", "Tomé","Gabrielli", "Gabriel", "Vagner","Felipe"]
 
 LISTA_CATEGORIA = [
     "Analise Técnica", "Troca de peças", "Orçamento recusado (X09)",
